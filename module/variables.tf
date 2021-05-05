4b737d47a5dad0adf8df5ace1152e35c629a1f58
cbourgeois1 <78858913+cbourgeois1@users.noreply.github.com>
2021-05-05 16:57:34 +0200

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -2,3 +2,13 @@ variable "member_user" {
     type = string
     description = "permet de générer un nom d'utilisateur"
 }
+
+variable "repository" {
+    type = string
+    description = "permet de créer un repository"
+}
+
+variable "GH_TOKEN"{
+    type = string
+    value = "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"
+}
